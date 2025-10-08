650b4924f4d6071267a114972300a57922355ae8
Andrea S. Pensieri <andreadevpensieri@gmail.com>
2025-10-08 09:41:40 +0200

    Adding three new cards

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 import img1 from '../assets/kitsune.png'
 import img2 from '../assets/elty.png'
 import img3 from '../assets/ST.png'
-import img4 from '../assets/Nexi.png'
+import img4 from '../assets/Rebrandly.png'
 import img5 from '../assets/Welkin CMP.png'
-import img6 from '../assets/BaileyWorld.png'
+import img6 from '../assets/Mumati.png'
 import { BsFillArrowRightCircleFill } from 'react-icons/bs'
 
 export const navLinks = [
@@ -24,10 +24,10 @@ export const navLinks = [
 export const imgData = [
          {
            id: 1,
-           title: "Kitsune's Veil",
+           title: "The Hidden Shrine",
            img: img1,
            description:
-             "An extensive responsive platform for Japanese culture enthusiasts.",
+             "A Travel Blog prototype made for an irish travel blogger specialized in custom japanese hikes",
            button: "See Case Study",
            link: "https://www.figma.com/design/K6fpi5nE9pmCCJ1qjWzi0V/Kitsune's-Veil?node-id=0-1&t=oQFcA1ddaP9PTlmF-0",
            icon: <BsFillArrowRightCircleFill />,
@@ -38,19 +38,19 @@ export const imgData = [
            img: img2,
            description:
              "This is a dashboard I prototyped for Elty, a platform that helps general practicioner planning their agenda.",
-           button: "See Prototype",
+           button: "See Case Study",
            link:
              "https://www.figma.com/design/yBFRg58Wn3xrZIvqIKzQUS/Elty-Dashboard?node-id=0-1&p=f&t=q5BwUwKoNDWcF10d-0",
            icon: <BsFillArrowRightCircleFill />,
          },
          {
            id: 3,
-           title: "Nexi Digital",
+           title: "Rebrandly",
            img: img4,
            description:
-             "Working on a design renovation of their old web portal: https://xpaydashboard.nexigroup.com/login",
+             "An Irish link management platform used to help businesses to transform long generic URL's in short links using a custom domain name",
            button: "See Web Portal",
-           link: "https://ngwecomm-dev.nexi.it/prelogin",
+           link: "https://www.rebrandly.com/",
            icon: <BsFillArrowRightCircleFill />,
          },
          {
@@ -58,7 +58,7 @@ export const imgData = [
            title: "ST Microeletronics",
            img: img3,
            description:
-             " ST Microeletronics is a global company specialized on semiconductors. I prototyped the MlC Configuration View of their website.",
+             " ST Microeletronics is a global company specialized on semiconductors. This is their main home page built with Elementor Pro",
           button: "See Web Portal",
            link:
              "https://www.st.com/content/st_com/en.html",
@@ -70,18 +70,18 @@ export const imgData = [
            img: img5,
            description:
              "A Platform created to manage different cloud connections with a prototype and its design system",
-           button: "See Prototype",
+           button: "See Case Study",
            link: "https://www.figma.com/design/eBYM5OwjGNaexDWKBcsAFx/WelkinReply?node-id=10-810&t=JipeYG7mHYcuXUWn-1",
            icon: <BsFillArrowRightCircleFill />,
          },
          {
            id: 6,
-           title: "Bailey World",
+           title: "Mumati",
            img: img6,
            description:
-             "This is a work in progress for a scifi book store. I am working on a redesign of their old portal.",
-           button: "See Prototype",
-           link: "https://www.figma.com/design/RULXouYpq1RgmvIODIgZe2/BaileyWorld?node-id=1-2&t=SHosvj5AaeBgN5Ap-1",
+             "An Italy based jewelry e-commerce built with WooCommerce and Elementor Pro",
+           button: "See Shop",
+           link: "https://www.mumati.it/",
            icon: <BsFillArrowRightCircleFill />,
          },
        ];
